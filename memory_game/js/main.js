fbd9f8390c2228469ebf27d01e082494e62175fc
David Zinser <dzinser@dmzmbp.attlocal.net>
2019-06-02 12:27:29 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,20 +1,44 @@
 console.log("Up and running!");
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+	{
+		rank: "Queen",
+		suit: "Hearts",
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank: "Queen",
+		suit: "Diamonds",
+		cardImage: "images/queen-of-diamonds.png",
+	},
+	{
+		rank: "King",
+		suit: "Hearts",
+		cardImage: "images/king-of-hearts.png",
+	},
+	{
+		rank: "King",
+		suit: "Diamonds",
+		cardImage: "images/king-of-diamonds.png",
+	}
+	];
 var cardsInPlay = [];
 
 function checkForMatch() {
 
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		console.log("You've Found A Match!");
+		alert("You've Found A Match!");
 	} else {
-		console.log("Sorry, Try Again!");
+		alert("Sorry, Try Again!");
 	}
 }
 
 function flipcard(cardId) {
 
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
